d88917e68324145721c8514e8bb6f1eca05fbba6
Máté Osztermayer <mazeventus@googlemail.com>
2020-11-19 18:47:59 +0100

    More realistic car + Spawnpoint
    Made carwheels rotate when moving,
    only drive left and right if its moving,
    made crate spawn tires and despawn

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Transform target;
 
     //offset from a local point of car to camera position
     private Vector3 offset = new Vector3(0, 5, -7);
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = player.transform;
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        //set the camera to the position of the car with offset
+        //transform.position = player.transform.position + offset;
+
+        //set the camera to the position of the car with offset, converted to World Space
+        transform.position = target.TransformPoint(offset);
+
+        //Look at your car's transorm position
+        transform.LookAt(target);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Public Variables
-    public float speed = 5.0f; // Speed for hot red vehicle
-    public float turnSpeed;
-    public float horizontalInput;
-    public float verticalInput; //Input of arrow keys up and down
+    public float speed; // Speed for hot red vehicle
+    public float turnSpeed; // Rotationspeed for hot red vehicle
+    private float horizontalInput; // Input for arrow keys left and right
+    private float verticalInput; // Input of arrow keys up and down
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -23,8 +27,37 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // Speed the shit outta Pallet Town with your car using forward, backwards, left and right
+
+
+        //INPUT FOR RIGIDBODY MOVEMENT
+
+        // if (Input.GetKey("w"))
+        {
+            //rb.velocity = transform.forward * speed;
+        }
+        //if (Input.GetKey("s"))
+        {
+            //rb.velocity = -transform.forward * speed;
+        }
+
+
+
+        // Speed the shit outta Pallet Town with your car using forward, backwards
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (verticalInput != 0)
+        {
+            // If TRUE, e.g. if up or down arrow keys are pressed, do:
+            //Turn our hot red vehicle, when pressing left and right keys
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput, Space.Self); 
+
+            Debug.Log("Vertical Input ist größer als Null!" + verticalInput + "Interessant!");
+            Debug.Log(verticalInput);
+        }
+        else
+        {
+            // If FALSE, e.g. if up or down keys are NOT pressed:
+            Debug.Log("Vertical Input ist gleich NULL!" + verticalInput);
+        }
     }
 }
